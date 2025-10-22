50fa6b27cec76aefb529bc6106dddbc7f79b4fd5
CharlesLightjarvis <charlestagne55@gmail.com>
2025-10-22 16:29:44 +0200

    add backend & gate and policy

---
+++ posttest/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('posts', PostController::class);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return response()->json([
+        'user' => UserResource::make($request->user()),
+    ]);
+});
